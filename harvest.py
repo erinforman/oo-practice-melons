c473c466bdd8de08d0bee7289a2bc44b065e678c
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-17 16:12:43 -0800

    complete make melong types function

--- harvest.py
+++ harvest.py
@@ -9,19 +9,24 @@ class MelonType(object):
     def __init__(self, code, first_harvest, color, is_seedless, is_bestseller, 
                  name):
         """Initialize a melon."""
-
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless  = is_seedless
+        self.is_bestseller = is_bestseller
         self.pairings = []
+        self.name = name
 
         # Fill in the rest
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
-
+        self.pairings.extend(pairing)
         # Fill in the rest
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
-
+        self.code = new_code
         # Fill in the rest
 
 
@@ -30,14 +35,31 @@ def make_melon_types():
 
     all_melon_types = []
 
-    # Fill in the rest
+    musk = MelonType('musk', 1998, 'green', True, True, "Muskmelon")
+    musk.add_pairing('mint')
+    all_melon_types.append(musk)
+
+    cas = MelonType('cas', 2003, 'orange', True, False, "Casaba")
+    cas.add_pairing(['strawberries', 'mint'])
+    all_melon_types.append(cas)
+
+    cren = MelonType('cren', 1996, 'green', True, False, "Crenshaw")
+    cren.add_pairing("proscuitto")
+    all_melon_types.append(cren)
+
+    yw = MelonType('yw', 2013, 'yellow', True, True, "Yellow Watermelon")
+    yw.add_pairing("ice cream")
+    all_melon_types.append(yw)
 
     return all_melon_types
 
-def print_pairing_info(melon_types):
-    """Prints information about each melon type's pairings."""
+    #return type(all_melon_types)
 
-    # Fill in the rest
+def print_pairing_info(all_melon_types):
+    """Prints information about each melon type's pairings."""
+    for melon_type in all_melon_types:
+        print(f'{melon_type.name}')
+
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
@@ -66,3 +88,4 @@ def get_sellability_report(melons):
 
 
 
+all_melon_types = make_melon_types()
